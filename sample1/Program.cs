b67fa59b96151598d72ab3126d940b87af2af847
Gabriel Landman <gabpimento@Gabriels-MacBook-Pro.local>
2026-03-29 12:23:47 +0200

    add sample1 hello world with classe personne

---
+++ sample1/Program.cs
@@ -0,0 +1,16 @@
+namespace sample1;
+
+class Personne
+{
+    public string Nom { get; set; } = "";
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var personne = new Personne { Nom = "Gabriel", Age = 22 };
+        Console.WriteLine($"hello {personne.Nom}, you are {personne.Age}");
+    }
+}
